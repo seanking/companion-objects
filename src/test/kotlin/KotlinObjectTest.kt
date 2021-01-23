b83b8a9635e5f8b70b281b2ba586e50f286499c1
Sean King <sking@ingeniaservices.com>
2021-01-23 17:37:31 -0500

    Introduce Test for Kotlin Object

---
+++ src/test/kotlin/KotlinObjectTest.kt
@@ -0,0 +1,20 @@
+import assertk.assertThat
+import assertk.assertions.isSameAs
+import org.junit.jupiter.api.Test
+
+class KotlinObjectTest {
+    
+    @Test
+    fun `should be a singleton`() {
+        // Given
+        val firstObject = KotlinObject
+        
+        // When 
+        val secondObject = KotlinObject
+        
+        // Then
+        assertThat(firstObject).isSameAs(secondObject)
+    }
+}
+
+object KotlinObject
