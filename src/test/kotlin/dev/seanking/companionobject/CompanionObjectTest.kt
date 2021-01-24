c0d279d5ae7e45c3708fdf9c5500dbbc1c889ccf
Sean King <sking@ingeniaservices.com>
2021-01-24 17:35:16 -0500

    Fix Formatting

--- src/test/kotlin/dev/seanking/companionobject/CompanionObjectTest.kt
+++ src/test/kotlin/dev/seanking/companionobject/CompanionObjectTest.kt
@@ -1,3 +1,5 @@
+package dev.seanking.companionobject
+
 import assertk.all
 import assertk.assertThat
 import assertk.assertions.isEqualTo
@@ -9,31 +11,31 @@ class CompanionObjectTest {
     fun `should provide singleton for class `() {
         // When
         val person = Person.defaultPerson()
-        
+
         // Then 
-        assertThat(person).all { 
+        assertThat(person).all {
             prop(Person::firstName).isEqualTo("Jane")
             prop(Person::lastName).isEqualTo("Smith")
         }
     }
-    
+
     @Test
     fun `should implement an interface`() {
         // When 
         val car = Car.create()
-        
+
         // Then
-        assertThat(car).all { 
+        assertThat(car).all {
             prop(Car::make).isEqualTo("Honda")
             prop(Car::model).isEqualTo("Civic")
         }
-    }     
-    
+    }
+
     @Test
     fun `should generate real static methods`() {
         // When
         val dog = Dog.defaultDog()
-        
+
         // Then
         assertThat(dog).prop(Dog::breed).isEqualTo("mutt")
     }
